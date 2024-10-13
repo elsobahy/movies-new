e4f90d2aa3df5d8d769ab87b7685a733b08fcf0d
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-14 01:34:50 +0300

    Change api to supabase and save the supabase url and key at constants folder for best practice
    then change the axios request to the new api

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Joi from 'joi';
-import axios from 'axios';
-import './login.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Joi from "joi";
+import axios from "axios";
+import "./login.css";
+import { SUPABASE_KEY, SUPABASE_URL } from "../../constants";
 
 export default function Login({ saveUserData }) {
   const navigate = useNavigate();
   const [errors, setErrors] = useState("");
   const [validErrors, setValidErrors] = useState([]);
   const [formData, setFormData] = useState({
-    email: "",  
-    password: ""
+    email: "",
+    password: "",
   });
 
   function getData(e) {
@@ -22,21 +23,34 @@ export default function Login({ saveUserData }) {
   function handleLogin(e) {
     e.preventDefault();
     let checkErrors = handleValidation();
-    
+
     if (checkErrors?.error) {
       setValidErrors(checkErrors.error.details);
     } else {
-      axios.post('https://hawas.runasp.net/api/v1/Login', formData)
+      const response = axios
+        .post(
+          `${SUPABASE_URL}/auth/v1/token?grant_type=password`,
+          {
+            email: formData.email,
+            password: formData.password,
+          },
+          {
+            headers: {
+              apikey: SUPABASE_KEY,
+              "Content-Type": "application/json",
+            },
+          }
+        )
         .then((res) => {
-            
-          localStorage.setItem("my-token", res.data.jwt);
+          console.log(res);
+          localStorage.setItem("my-token", res.data.access_token);
           saveUserData();
           setValidErrors([]);
-          navigate('/home');
+          navigate("/home");
           setErrors("");
         })
         .catch((err) => {
-          setErrors(err.response?.data?.message || "An error occurred");
+          setErrors(err.response?.data?.message || "The email or password is incorrect.");
           setValidErrors([]);
         });
     }
@@ -44,40 +58,54 @@ export default function Login({ saveUserData }) {
 
   function handleValidation() {
     let schema = Joi.object({
-      email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+      email: Joi.string().email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      }),
+      password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
     });
-    
+
     return schema.validate(formData, { abortEarly: false });
   }
 
   return (
-    <div className='container w-75 text-start mx-auto mt-5'>
-      <h3 className='text-center'>Login Now</h3>
-      {errors && <h6 className='alert alert-danger'>{errors}</h6>}
-      {validErrors.length > 0 && validErrors.map((error, index) => (
-        <h6 key={index} className='alert alert-danger'>{error.message}</h6>
-      ))}
+    <div className="container w-75 text-start mx-auto mt-5">
+      <h3 className="text-center">Login Now</h3>
+      {errors && <h6 className="alert alert-danger">{errors}</h6>}
+      {validErrors.length > 0 &&
+        validErrors.map((error, index) => (
+          <h6 key={index} className="alert alert-danger">
+            {error.message}
+          </h6>
+        ))}
       <form onSubmit={handleLogin}>
-        <label htmlFor='email' className='form-label'>Email :</label>
+        <label htmlFor="email" className="form-label">
+          Email :
+        </label>
         <input
-          type='email'
-          className='form-control mb-3'
-          id='email'
+          type="email"
+          className="form-control mb-3"
+          id="email"
           name="email"
           value={formData.email}
-          onChange={getData} />
-        
-        <label htmlFor='password' className='form-label'>Password :</label>
+          onChange={getData}
+        />
+
+        <label htmlFor="password" className="form-label">
+          Password :
+        </label>
         <input
-          type='password'
-          className='form-control mb-3'
-          id='password'
+          type="password"
+          className="form-control mb-3"
+          id="password"
           name="password"
           value={formData.password}
-          onChange={getData} />
-        
-        <button type='submit' className='btn btn-info'>Login</button>
+          onChange={getData}
+        />
+
+        <button type="submit" className="btn btn-info">
+          Login
+        </button>
       </form>
     </div>
   );
---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://erieaeemvxwsqgaqadrc.supabase.co";
+export const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyaWVhZWVtdnh3c3FnYXFhZHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg4NTczNTQsImV4cCI6MjA0NDQzMzM1NH0.j_rex18MOV8CASYW1j2sNZ9gVBwXcncLwBr3w7kRLJE";
